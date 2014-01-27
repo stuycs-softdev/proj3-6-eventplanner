d21c0e4d244da146739939eb434dfe860bf247db
janejoyce <jja8@me.com>
2014-01-27 10:28:00 -0500

    modifying app.py to properly display search page

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
                 return redirect("/search")
 
 
-@app.route("/register", methods = ["GET", "POST"]) #HELP
+@app.route("/register", methods = ["GET", "POST"])
 def register():
      if request.method == "GET":
          return render_template("register.html")
@@ -57,8 +57,10 @@ def logout():
 
         
 
-@app.route("/search")
+@app.route("/search", methods = ["GET", "POST"])
 def search():
+    if request.method == "GET":
+	return render_template("search.html")
     start = request.form.get("1","")
     end = request.form.get("2","")
     if start == None or end == None:
@@ -66,7 +68,7 @@ def search():
     try:
         results = yelp.search(food, location)
     except KeyError:
-        return render_template("route.html")
+        return render_template("results.html")
     session["results"] = results
     return redirect("results")
     
